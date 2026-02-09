733bd8a3d940ee2db1ba81a7852b421519086bc0
Lorenz Cleymans <lorenzcleymans@hotmail.com>
2026-02-09 21:57:17 +0100

    fix: remove Content-Type header for FormData in axios interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -31,6 +31,9 @@ apiClient.interceptors.request.use(
     if (token && config.headers) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+    if (config.data instanceof FormData) {
+      delete config.headers['Content-Type'];
+    }
     return config;
   },
   (error: AxiosError) => {
